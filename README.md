39042308c0cf7cdc6bc20e24f33efc2cf775860d
Rob <cloud.architecture2023@gmail.com>
2023-05-27 12:20:47 -0400

    adding first verrion of dir removal script

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# roboto
-First repo
-First commit
+# 					Basic Bash Scripts 
+# dirremovaltool.sh does the follopwing:
+# 1. List directories 
+# 2. Asks which directory/s you would like to remove
+# 3. If directory/s you input do not exist, it'll display an eror message and stop
+# 4. Lists the contents of the directory/s selected (if they exist^^)
+# 4. Asks if you are sure you'd like to remove the selected directory/s and all of their contents
+# 5. Removes directory/s and displays a successful message
---
+++ dirremovaltool.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "this are your current directories:"
+ls -l
+sleep 1
+echo "Enter the directoies you want to remove (separated by spaces):"
+read -a directories
+
+for dir in "${directories[@]}"; do
+    if [ ! -d "$dir" ]; then
+        echo "Directory not found: $dir"
+else
+    echo "These are the contents of $dir:"
+    ls $dir
+    read -p "Are you sure you want to continue? <y/n> " prompt
+    if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+        echo -n "Removing directory $dir..."
+	for ((i=0; i<=10; i++)); do
+            echo -ne "Progress: ["
+            for ((j=0; j<=i; j++)); do
+                echo -ne "#"
+            done
+            for ((j=i+1; j<=10; j++)); do
+                echo -ne " "
+            done
+            echo -ne "] $((i*10))%"
+            sleep 0.1
+            echo -ne "\r"
+        done
+	
+	rm -r "$dir"
+        echo "Directory $dir removed."
+    fi
+fi
+done
